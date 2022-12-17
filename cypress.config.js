eb79f7a9f7ba46bdfe30399d4ef461eb6dc6cc96
Matt Schile <mschile@cypress.io>
2022-12-16 17:46:27 -0700

    use HeapProfiler

--- cypress.config.js
+++ cypress.config.js
@@ -3,16 +3,6 @@ module.exports = {
   video: false,
   e2e: {
     baseUrl: 'https://earth.google.com/web/',
-    setupNodeEvents(on, config) {
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        if (browser.name === 'chrome' || browser.name === 'chromium') {    
-          launchOptions.args.push('--js-flags=--expose-gc')
-          // launchOptions.args.push('--enable-logging=stderr')
-          // launchOptions.args.push('--v=1')
-        }
-
-        return launchOptions
-      })
-    },
+    setupNodeEvents(on, config) {},
   },
 }
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,7 +1,3 @@
 afterEach(() => {
-  if (window.top.gc && Cypress.env('FORCE_GC') === true) {
-    for (let index = 0; index < 5; index++) {
-      window.top.gc()
-    }
-  }
+  Cypress.automation('remote:debugger:protocol', { command: 'HeapProfiler.collectGarbage' })
 })
